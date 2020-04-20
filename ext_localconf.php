40d160fbcd2cebdeef3b2bf3df0929df3153ff23
Benjamin Kott <benjamin.kott@outlook.com>
2020-04-20 23:22:42 +0200

    [BUGFIX] Do not use $_EXTKEY

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,7 +12,9 @@ defined('TYPO3_MODE') || die();
 /***************
  * PageTS
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TsConfig/Page/All.tsconfig">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:syntax/Configuration/TsConfig/Page/All.tsconfig">'
+);
 
 /***************
  * Register Icon
